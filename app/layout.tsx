d674560777ff746a2afa7c92d8ad9e222d7f36a8
Rahat Newaz Sikder <mailtorahat@proton.me>
2025-03-23 13:28:25 +0600

    metadata image path changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,18 +19,18 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Rahat's Portfolio - Web Developer | Frontend & Backend Projects",
+  title: "Rahat Newaz Sikder's Portfolio | Full Stack Developer",
   description:
-    "Explore the portfolio of Rahat, a skilled full-stack web developer showcasing innovative frontend and backend projects using React, Next.js, Node.js, PostgreSQL, and more.",
+    "Explore the portfolio of Rahat, a skilled full-stack developer showcasing innovative frontend and backend projects using React, Next.js, Node.js, PostgreSQL, and more.",
   openGraph: {
-    title: "Rahat's Portfolio - Web Developer | Full-Stack Projects",
+    title: "Rahat's Portfolio | Full Stack Developer",
     description:
       "Explore the portfolio of Rahat, featuring full-stack web development projects with cutting-edge technologies like React, Next.js, Node.js, and PostgreSQL.",
     url: "https://rahatsikder.com", // Replace with your website's URL
     siteName: "Rahat's Portfolio",
     images: [
       {
-        url: "/public/profile.jpg", // Replace with a path to a relevant image for social media
+        url: "/profile.jpg", // Replace with a path to a relevant image for social media
         width: 1200,
         height: 630,
         alt: "Rahat's Portfolio",
@@ -48,7 +48,7 @@ export const metadata: Metadata = {
     // image: "https://your-website-url.com/og-image.jpg", // Replace with the URL to your image
   },
   keywords:
-    "Rahat, Portfolio, Web Developer, Full Stack Developer, React, Next.js, Node.js, PostgreSQL, Frontend, Backend, JavaScript, TypeScript, Web Development",
+    "Rahat, Rahat Newaz Sikder, Portfolio, Web Developer, Full Stack Developer, React, Next.js, Node.js, PostgreSQL, Frontend, Backend, JavaScript, TypeScript, Web Development",
   robots: "index, follow",
 };
 
