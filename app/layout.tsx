ce1829f0cdb498a40ee094f0c55f3a35168a7a49
Rahat Newaz Sikder <mailtorahat@proton.me>
2025-12-10 13:26:47 +0600

    feat: Migrate to pnpm, update dependencies, and establish Next.js app router layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,47 +1,48 @@
-import type { Metadata } from "next";
-import { Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-  display: "swap",
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+  display: 'swap',
   adjustFontFallback: false,
-  weight: ["400", "500", "600", "700"],
+  weight: ['400', '500', '600', '700'],
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://rahatsikder.com'),
   title: "Rahat Newaz Sikder's Portfolio | Full Stack Developer",
   description:
-    "Explore the portfolio of Rahat, a skilled full-stack developer showcasing innovative frontend and backend projects using React, Next.js, Node.js, PostgreSQL, and more.",
+    'Explore the portfolio of Rahat, a skilled software developer showcasing innovative frontend and backend projects using React, Next.js, React Native, Node.js, PostgreSQL, and more.',
   openGraph: {
     title: "Rahat's Portfolio | Full Stack Developer",
     description:
-      "Explore the portfolio of Rahat, featuring full-stack web development projects with cutting-edge technologies like React, Next.js, Node.js, and PostgreSQL.",
-    url: "https://rahatsikder.com", // Replace with your website's URL
+      'Explore the portfolio of Rahat, featuring full-stack development projects with cutting-edge technologies like React, Next.js, React Native, Node.js, and PostgreSQL.',
+    url: 'https://rahatsikder.com', // Replace with your ite's URL
     siteName: "Rahat's Portfolio",
     images: [
       {
-        url: "/profile.jpg", // Replace with a path to a relevant image for social media
+        url: '/profile.jpg', // Replace with a path to a relevant image for social media
         width: 1200,
         height: 630,
         alt: "Rahat's Portfolio",
       },
     ],
-    type: "website",
+    type: 'website',
   },
   twitter: {
-    card: "summary_large_image",
-    site: "@your-twitter-handle", // Replace with your Twitter handle
-    creator: "@your-twitter-handle", // Replace with your Twitter handle
+    card: 'summary_large_image',
+    site: '@rahatsikder', // Replace with your Twitter handle
+    creator: '@rahatsikder', // Replace with your Twitter handle
     title: "Rahat's Portfolio | Full Stack Developer",
     description:
-      "Explore Rahat's web development portfolio featuring full-stack projects with React, Next.js, Node.js, and PostgreSQL.",
+      "Explore Rahat's portfolio featuring full-stack projects with React, Next.js, React Native, Node.js, and PostgreSQL.",
     // image: "https://your-website-url.com/og-image.jpg", // Replace with the URL to your image
   },
   keywords:
-    "Rahat, Rahat Newaz Sikder, Portfolio, Web Developer, Full Stack Developer, React, Next.js, Node.js, PostgreSQL, Frontend, Backend, JavaScript, TypeScript, Web Development",
-  robots: "index, follow",
+    'Rahat, Rahat Newaz Sikder, Portfolio, Web Developer, Full Stack Developer, React, Next.js, React Native, Node.js, PostgreSQL, Frontend, Backend, JavaScript, TypeScript, Web Development',
+  robots: 'index, follow',
 };
 
 export default function RootLayout({
@@ -50,7 +51,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body
         className={` ${geistMono.className}  flex min-h-screen flex-col items-center justify-center bg-foreground`}
       >
